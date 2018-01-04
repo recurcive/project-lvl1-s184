c94811af1e233e3887b4430a3dfb4e3ad2e360d0
Dmitry <recurcive@gmail.com>
2018-01-04 21:20:46 +0300

    Add game 'Parity check'

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
-import greetingUser from './index';
+// import greetingUser from './index';
+import checkEvenNumber from './brain-even';
 
-greetingUser();
+
+// greetingUser();
+checkEvenNumber();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import readlineSync from 'readline-sync';
 
 const greetingUser = () => {
-  console.log('Welcome to the Brain Games!\n');
+//  console.log('Welcome to the Brain Games!\n');
 
   const nameUser = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nameUser}`);
+  return nameUser;
 };
 
 export default greetingUser;
